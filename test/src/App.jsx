c293fb9481aad92dc312760459a20a9c599a2710
Ravinder singh <negiravi451@gmail.com>
2025-12-29 00:04:27 +0530

    updated

--- test/src/App.jsx
+++ test/src/App.jsx
@@ -3,16 +3,7 @@ import React from "react";
 const App = () => {
   return (
     <div>
-      this is staging now
-
-      <p>
-        ok so thats new changes for developmnents brabc chekc this easily
-        <span>
-          good to see this you have seen that changes made by me now chekc hti si developerm
-        </span>
-      </p>
-      <p>this is befor code</p>
-      <h1>ye mere pahle ka code hai gitb ke aane se pahle ka</h1>
+   <h1> this is staging branch</h1>
     </div>
   );
 };
